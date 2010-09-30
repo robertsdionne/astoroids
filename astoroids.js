beb56d2a3ce4b1d76f77230c6ef6b46753422143
Robert Dionne <robertsdionne@gmail.com>
2010-09-30 08:42:53 -0400

    Adding a flat rendering mode. Correctly handle wrapping the screen.

--- astoroids.js
+++ astoroids.js
@@ -1,5 +1,72 @@
 // Copyright 2010 Robert Scott Dionne. All Rights Reserved.
 
+var big = [
+  [ 0.00000, 0.03753,
+    0.00000, 0.09383,
+    0.02410, 0.15013,
+    0.06225, 0.13137,
+    0.08635, 0.15282,
+    0.11044, 0.11260,
+    0.06225, 0.07775,
+    0.11245, 0.05362,
+    0.11245, 0.03753,
+    0.07028, 0.00000,
+    0.02811, 0.00000,
+    0.04217, 0.03753 ],
+
+  [ 0.00000, 0.03485,
+    0.00000, 0.11796,
+    0.02209, 0.15013,
+    0.07028, 0.15013,
+    0.11044, 0.10992,
+    0.09438, 0.06702,
+    0.11044, 0.02681,
+    0.08434, 0.00000,
+    0.05422, 0.02949,
+    0.02610, 0.00000 ],
+
+  [ 0.00000, 0.05898,
+    0.02811, 0.07239,
+    0.00000, 0.09383,
+    0.02209, 0.15013,
+    0.05020, 0.08311,
+    0.04217, 0.15013,
+    0.07028, 0.15013,
+    0.11245, 0.08311,
+    0.11245, 0.05362,
+    0.08434, 0.00000,
+    0.04016, 0.00000 ],
+
+  [ 0.00000, 0.04021,
+    0.01406, 0.07507,
+    0.00000, 0.11528,
+    0.02811, 0.15013,
+    0.04016, 0.13137,
+    0.07631, 0.15013,
+    0.11044, 0.09383,
+    0.07831, 0.05630,
+    0.11044, 0.04021,
+    0.08233, 0.00000,
+    0.05422, 0.02145,
+    0.03414, 0.00000 ]
+];
+
+var subdivideLoop = function(k, points) {
+  var out = [];
+  var m = points.length;
+  for (var i = 0; i < m; i += 2) {
+    var x0 = points[i % m];
+    var y0 = points[(i+1) % m];
+    var x1 = points[(i+2) % m];
+    var y1 = points[(i+3) % m];
+    for (var j = 0; j < k; ++j) {
+      out.push(x0 + j * (x1 - x0) / k);
+      out.push(y0 + j * (y1 - y0) / k);
+    }
+  }
+  return out;
+};
+
 var keys = {
   up: false,
   down: false,
@@ -9,6 +76,7 @@ var keys = {
 };
 
 var keydown = function(e) {
+  if (!e.which) {e.which = e.keyCode;}
   switch(e.which) {
     case 32:
       keys.fire = true;
@@ -32,6 +100,7 @@ var keydown = function(e) {
 };
 
 var keyup = function(e) {
+  if (!e.which) {e.which = e.keyCode;}
   switch(e.which) {
     case 32:
       keys.fire = false;
@@ -126,7 +195,7 @@ var update = function() {
   YV *= 0.995;
 };
 
-var N = 3;
+var N = 23;
 var M = 8000;
 
 var onCreate = function(gl, p, b, g, asteroid) {
@@ -156,11 +225,17 @@ var onCreate = function(gl, p, b, g, asteroid) {
   p.abRho = gl.getUniformLocation(p, 'abRho');
   p.position = gl.getAttribLocation(p, 'position');
 
-  var data = [
-    -0.01, -0.01,
-     0.01,  0.00,
-    -0.01,  0.01
-  ];
+  var data = [];
+  for (var i = 0; i < 10; ++i) {
+    data.push(-0.03217 + i * 2.0 * 0.03217 / 10.0);
+    data.push(-0.01406 + i * 0.01406 / 10.0);
+  }
+  for (var i = 0; i < 11; ++i) {
+    data.push(0.03217 + i * -2.0 * 0.03217 / 10.0);
+    data.push(i * 0.01406 / 10.0);
+  }
+  data.push(data[36], data[37]);
+  data.push(data[4], data[5]);
 
   var a = new Float32Array(data);
   gl.bindBuffer(gl.ARRAY_BUFFER, b);
@@ -177,14 +252,7 @@ var onCreate = function(gl, p, b, g, asteroid) {
   gl.bufferData(gl.ARRAY_BUFFER, d.byteLength, gl.STATIC_DRAW);
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, d);
 
-  var data3 = [];
-  for (var i = 0; i < 24; ++i) {
-    data3.push(
-        0.01 * Math.cos(i * 2.0 * pi / 24.0),
-        0.01 * Math.sin(i * 2.0 * pi / 24.0));
-  }
-
-  var asteroidBuffer = new Float32Array(data3);
+  var asteroidBuffer = new Float32Array(subdivideLoop(10, big[3]));
   gl.bindBuffer(gl.ARRAY_BUFFER, asteroid);
   gl.bufferData(gl.ARRAY_BUFFER, asteroidBuffer.byteLength, gl.STATIC_DRAW);
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, asteroidBuffer);
@@ -197,10 +265,14 @@ var onChange = function(gl, width, height) {
 
 var abRho = new Float32Array([0.32, 0.44, 0.125]);
 
+var wrap = function(coord) {
+  return coord - Math.floor(coord);
+};
+
 var onDraw = function(gl, p, b, g, asteroid) {
   gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
   gl.bindBuffer(gl.ARRAY_BUFFER, b);
-  var xyChi = new Float32Array([X, Y, CHI]);
+  var xyChi = new Float32Array([wrap(X), wrap(Y), CHI]);
   gl.uniform3fv(p.xyChi, xyChi);
   gl.uniform3fv(p.abRho, abRho);
   gl.vertexAttribPointer(p.position, 2, gl.FLOAT, false, 0, 0);
@@ -208,19 +280,19 @@ var onDraw = function(gl, p, b, g, asteroid) {
   gl.drawArrays(gl.LINE_STRIP, 0, N);
   gl.disableVertexAttribArray(p.position);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, g);
-  var xyChi = new Float32Array([-0.02 * X, -0.02 * Y, 0.0]);
-  gl.uniform1f(p.size, 0.7);
-  gl.uniform3fv(p.xyChi, xyChi);
-  gl.uniform3fv(p.abRho, abRho);
-  gl.vertexAttribPointer(p.position, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(p.position);
-  gl.drawArrays(gl.POINTS, 0, M);
-  gl.disableVertexAttribArray(p.position);
-  gl.flush();
+//gl.bindBuffer(gl.ARRAY_BUFFER, g);
+//var xyChi = new Float32Array([wrap(-0.02 * X), wrap(-0.02 * Y), 0.0]);
+//gl.uniform1f(p.size, 0.7);
+//gl.uniform3fv(p.xyChi, xyChi);
+//gl.uniform3fv(p.abRho, abRho);
+//gl.vertexAttribPointer(p.position, 2, gl.FLOAT, false, 0, 0);
+//gl.enableVertexAttribArray(p.position);
+//gl.drawArrays(gl.POINTS, 0, M);
+//gl.disableVertexAttribArray(p.position);
+//gl.flush();
 
   gl.bindBuffer(gl.ARRAY_BUFFER, g);
-  var xyChi = new Float32Array([BX, BY, 0.0]);
+  var xyChi = new Float32Array([wrap(BX), wrap(BY), 0.0]);
   gl.uniform1f(p.size, 2.5);
   gl.uniform3fv(p.xyChi, xyChi);
   gl.uniform3fv(p.abRho, abRho);
@@ -230,13 +302,13 @@ var onDraw = function(gl, p, b, g, asteroid) {
   gl.disableVertexAttribArray(p.position);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, asteroid);
-  var xyChi = new Float32Array([asteroidX, asteroidY, 0.0]);
+  var xyChi = new Float32Array([wrap(asteroidX), wrap(asteroidY), 0.0]);
   gl.uniform1f(p.size, 1.0);
   gl.uniform3fv(p.xyChi, xyChi);
   gl.uniform3fv(p.abRho, abRho);
   gl.vertexAttribPointer(p.position, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(p.position);
-  gl.drawArrays(gl.LINE_LOOP, 0, 24);
+  gl.drawArrays(gl.LINE_LOOP, 0, 110);
   gl.disableVertexAttribArray(p.position);
   gl.flush();
 };
